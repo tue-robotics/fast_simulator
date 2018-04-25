52234e9fc030be7d9c62d4d07de8b9249773fe65
Loy van Beek <loy.vanbeek@gmail.com>
2018-04-25 12:23:00 +0200

    Initialize neck joints for tilt and pan

--- src/Amigo.cpp
+++ src/Amigo.cpp
@@ -139,6 +139,8 @@ Amigo::Amigo(ros::NodeHandle& nh) : Robot(nh, "amigo")
 //    body_.addActionServer(nh, "right_arm/joint_trajectory_action");
 
     body_.initJoint("torso_joint", 0.35);
+    body_.initJoint("neck_pan_joint", 0.0);
+    body_.initJoint("neck_tilt_joint", 0.0);
     body_.initJoint("shoulder_yaw_joint_left", -0.01);
     body_.initJoint("shoulder_pitch_joint_left", -0.4);
     body_.initJoint("shoulder_roll_joint_left", 0);
