045b26bb88d23aa8395aae25dc8e0e40cc0dcc4b
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2019-02-21 21:57:28 +0100

    fix env-hook generation

--- env-hooks/60.fast_simulator.bash
+++ env-hooks/60.fast_simulator.bash
@@ -1,5 +1,4 @@
-sim-console ()
-{
+sim-console () {
     SIM_CONSOLE_DIR=`rospack find fast_simulator`/src/fast_simulator
     ipython -i --no-banner --no-confirm-exit --autocall 2 "${SIM_CONSOLE_DIR}/console.py" -- $*
 }
